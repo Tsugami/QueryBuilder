477ccfdb2bf167c965c5ac095b0acabd3c674aab
Yslan Ramos <yslan3000@gmail.com>
2020-12-12 13:54:27 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'import/extensions': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+      typescript: {},
+    },
+  },
+};
---
+++ src/__test__/sum.test.ts
@@ -0,0 +1,5 @@
+import sum from '../sum';
+
+it('should return 4', () => {
+  expect(sum(2, 2)).toEqual(4);
+});
