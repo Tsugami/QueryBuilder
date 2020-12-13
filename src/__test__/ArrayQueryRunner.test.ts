14054ceb553d63644d6227391ef9c203f5ee3905
Yslan Ramos <yslan3000@gmail.com>
2020-12-13 02:37:22 -0300

    test: add more tests

--- src/__test__/ArrayQueryRunner.test.ts
+++ src/__test__/ArrayQueryRunner.test.ts
@@ -4,8 +4,13 @@ import ArrayQueryRunner from '../runners/ArrayQueryRunner';
 describe('ArrayQueryRunner', () => {
   const QueryRunner = new ArrayQueryRunner(mock);
 
-  it('should return one user with id 1', async () => {
+  it('should return the user with id 1', async () => {
     const user = await QueryRunner.createQueryBuilder().column('id').Equal(1).getOne();
     expect(user?.id).toEqual(1);
+  });
+
+  it('should return the first user that the id is not 1', async () => {
+    const user = await QueryRunner.createQueryBuilder().column('id').not.Equal(1).getOne();
+    expect(user?.id).toEqual(2);
   });
 });
